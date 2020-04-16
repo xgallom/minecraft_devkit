c15d14c75819e5225056ab39bad8029b11d53f71
Milan Gallo <gallo.milan.jr@gmail.com>
2020-04-16 05:24:35 +0200

    Implement register instructions

--- Compiler/Op.lua
+++ Compiler/Op.lua
@@ -5,27 +5,39 @@ local Op = {
     Address = 3,
 }
 
+local Parsers = {
+    [Op.Disabled] = nil,
+    [Op.Constant] = "#(%-?[abcdefx%d]+)",
+    [Op.Register] = function(token)
+        local register = string.match(token, "[ABCDEF]")
+
+        if register == nil then return nil end
+
+        return string.byte(register) - string.byte("A")
+    end,
+    [Op.Address ] = "$(%-?[abcdefx%d]+)"
+}
+
 function Op.parseImpl(parser, token)
     if token == nil then token = "" end
-    
-    return tonumber(string.match(token, parser))
+
+    if type(parser) == "string" then
+        return tonumber(string.match(token, parser))
+    elseif type(parser) == "function" then
+        return parser(token)
+    end
+
+    return nil
 end
 
 function Op.parse(op, token)
-    local parsers = {
-        [Op.Disabled] = nil,
-        [Op.Constant] = "#(%-?[abcdefx%d]+)",
-        [Op.Register] = "([ABCDE])",
-        [Op.Address ] = "$(%-?[abcdefx%d]+)"
-    }
-    
-    parser = parsers[op]
-    
+    local parser = Parsers[op]
+
     if parser == nil then
         if token == nil then return 0
         else return nil end
     end
-    
+
     return Op.parseImpl(parser, token)
 end
 
